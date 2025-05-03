6ac30a72839c406272285a1c6d68da4ff931d34a
Yurii <j.koroliak@gmail.com>
2025-05-03 14:39:38 +0200

    function for slider (u can change slides with arrows)

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,28 @@
 let slideIndex = 0;
 slideShow();
 
+function plusSlides(n) {
+
+  slideIndex += n;
+  //увеличивает или уменьшает текущий индекс слайда
+
+  let slides = document.getElementsByClassName('mySlide');
+  //масив обьектов с классом mySlide
+
+  if (slideIndex > slides.length) slideIndex = 1;
+  if (slideIndex < 1) slideIndex = slides.length;
+  //Если индекс становится больше числа слайдов, он возвращается на первый.
+  //Если меньше 1, переключается на последний слайд.
+
+  // Скрываем все слайды
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  // Показываем текущий
+  slides[slideIndex - 1].style.display = "flex";
+}
+
 function slideShow(){
     let slides = document.getElementsByClassName('mySlide');
     //йоу тут создал масив обьектов с классом 'mySlide'
@@ -28,11 +50,11 @@ function slideShow(){
 
 
     slides[slideIndex - 1].style.display = "flex";
-    //показываем один слайд из массива (даем ему display block)
+    //показываем один слайд из массива (даем ему display flex)
     //slideIndex - 1 потому что индексация в масиве начинается с 0 и если у нас slideIndex равняется 1 то надо отнять 1 что бы показать именно первый обэкт в масиве
 
 
-    setTimeout(slideShow, 3000);
+    setTimeout(slideShow, 4000);
     //йоу тут задал что бы слайды менялись каждые 3000 миллисекунд тоесть наш цикл будет запускаться раз в 3к миллисекунд
 }
 
